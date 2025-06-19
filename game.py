1899081e9ce1ab466ed04fc1a716ad4061a8d92c
BWatters53 <benjwatkinson@gmail.com>
2025-06-19 17:42:47 +0100

    Working Collisions

--- game.py
+++ game.py
@@ -7,16 +7,19 @@ pygame.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption("Bobbin Dud")
 display = pygame.Surface((400,400)) # 2x Scaling
 
 #Assets
-
+assets = {
+    "sky":loadImg("sky_bg"),
+}
 
 running = True
 while running:
     #Display Scaling
     screen.blit(pygame.transform.scale(display, (800,800)), (0,0))
-    display.blit()
+    display.blit(assets["sky"],(0,0))
 
     #Events
     for event in pygame.event.get():
